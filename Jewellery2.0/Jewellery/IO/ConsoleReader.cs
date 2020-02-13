40229976789fc4797b9821af28bf87ee8a145482
CodingMania <vasilmitovv@gmail.com>
2020-02-13 11:00:23 +0200

    Add ConsoleReader class

---
+++ Jewellery2.0/Jewellery/IO/ConsoleReader.cs
@@ -0,0 +1,20 @@
+﻿namespace GoldJewelry.IO
+{
+    using System;
+    using System.Text;
+
+    using Contracts;
+
+    public class ConsoleReader : IReader
+    {
+        public ConsoleReader()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+        }
+
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
