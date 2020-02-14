0093775741419d11ba8c82607d7f439e2c8ceab2
CodingMania <vasilmitovv@gmail.com>
2020-02-14 23:10:42 +0200

    Add Factory class

---
+++ Jewellery2.0/Jewellery/Models/Factory/JewelryFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace GoldJewelry.Models.Factory
+{
+    using Contracts;
+    using Models.Contracts;
+
+    public class JewelryFactory : IJewelryFactory
+    {
+        public IJewelry GetJewelry(string type, double weigth)
+        {
+            return new Jewelry(type, weigth);
+        }
+    }
+}
