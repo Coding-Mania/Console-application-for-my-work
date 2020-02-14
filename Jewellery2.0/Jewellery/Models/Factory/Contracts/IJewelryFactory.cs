409e8cfbb421346718f4a9b826e0a70e72b9676d
CodingMania <vasilmitovv@gmail.com>
2020-02-14 23:08:08 +0200

    Add Factory interface

---
+++ Jewellery2.0/Jewellery/Models/Factory/Contracts/IJewelryFactory.cs
@@ -0,0 +1,9 @@
+﻿using GoldJewelry.Models.Contracts;
+
+namespace GoldJewelry.Models.Factory.Contracts
+{
+    public interface IJewelryFactory
+    {
+        IJewelry GetJewelry(string type, double weigth);
+    }
+}
